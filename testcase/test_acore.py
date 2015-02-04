873ae849710001634ede7788ab5588a0a69994db
pastebt <maye1968@gmail.com>
2015-02-03 17:06:47 -0800

    pep8 check

--- testcase/test_acore.py
+++ testcase/test_acore.py
@@ -11,7 +11,7 @@ class FAKE_USER(acore.Acore):
     def read_all(self, arw, size=0, seps=()):
         for y in arw.aread(size, seps):
             yield y
-        
+
 
 class FAKE(acore.ARW):
     def __init__(self, data):
@@ -71,21 +71,21 @@ class TestARW(unittest.TestCase):
         ret = [y for y in f.read_all(seps=('\r\n',))]
         self.assertEqual(f.request_data, 'abc\r\n')
         self.assertEqual(f._data_buf, '1234\r\n56')
- 
+
     def test_read_all5(self):
         f = FAKE(("\n1234\r\n56",))
         f._data_buf = 'abc'
         ret = [y for y in f.read_all(seps=('\r\n',))]
         self.assertEqual(f.request_data, 'abc\n1234\r\n')
         self.assertEqual(f._data_buf, '56')
- 
+
     def test_read_all6(self):
         f = FAKE(("\r\n1234\r\n56",))
         f._data_buf = 'abc'
         ret = [y for y in f.read_all(seps=('\r\n',))]
         self.assertEqual(f.request_data, 'abc\r\n')
         self.assertEqual(f._data_buf, '1234\r\n56')
- 
+
     def test_read_all7(self):
         # Winner's index is smaller
         f = FAKE(("\n1234\r\n56",))
@@ -93,7 +93,7 @@ class TestARW(unittest.TestCase):
         ret = [y for y in f.read_all(seps=('\r\n', '\n'))]
         self.assertEqual(f.request_data, 'abc\n')
         self.assertEqual(f._data_buf, '1234\r\n56')
- 
+
     def test_read_all8(self):
         f = FAKE(("\n1234\r\n56",))
         f._data_buf = 'abc'
@@ -108,7 +108,7 @@ class TestARW(unittest.TestCase):
         self.assertEqual(f.sep_hit, '\n')
         self.assertEqual(f.request_data, 'abc\n')
         self.assertEqual(f._data_buf, '1234\r\n56')
- 
+
     def test_read_all10(self):
         f = FAKE(("\n1234\r\n56",))
         f._data_buf = 'abc'
@@ -116,7 +116,7 @@ class TestARW(unittest.TestCase):
         self.assertEqual(f.sep_hit, '\n')
         self.assertEqual(f.request_data, 'abc\n')
         self.assertEqual(f._data_buf, '1234\r\n56')
- 
+
     def test_read_all11(self):
         f = FAKE(("\r\n1234\r\n56",))
         f._data_buf = 'abc'
