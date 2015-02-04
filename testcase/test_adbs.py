a5c46bc124f58a6b0cbf9fabeedb7f702c6fec12
pastebt <maye1968@gmail.com>
2015-02-04 11:55:47 -0800

    merge from develop 873ae849710

--- testcase/test_adbs.py
+++ testcase/test_adbs.py
@@ -34,7 +34,6 @@ class T2(adbs.Table):
     pwd = adbs.Column('1234', name='passwd')
 
 
-
 class TestWhere(unittest.TestCase):
     def test_sql1(self):
         db = FakeDB()
@@ -81,7 +80,8 @@ class TestWhere(unittest.TestCase):
         w = (T1.uid > 1).AND("col2 >  1")
         self.assertEqual(w.sql(db), "(`uid` > 1 and col2 >  1)")
         w = (T1.uid > 1).AND("col2 >  1").AND(T1.name == 'a')
-        self.assertEqual(w.sql(db), "((`uid` > 1 and col2 >  1) and `name` = 'a')")
+        self.assertEqual(w.sql(db),
+                         "((`uid` > 1 and col2 >  1) and `name` = 'a')")
 
     def test_and2(self):
         db = FakeDB()
@@ -101,8 +101,9 @@ class TestWhere(unittest.TestCase):
         w = (T1.uid > 1).OR("col2 >  1")
         self.assertEqual(w.sql(db), "(`uid` > 1 or col2 >  1)")
         w = (T1.uid > 1).OR("col2 >  1").OR(T1.name == 'a')
-        self.assertEqual(w.sql(db), "((`uid` > 1 or col2 >  1) or `name` = 'a')")
-       
+        self.assertEqual(w.sql(db),
+                         "((`uid` > 1 or col2 >  1) or `name` = 'a')")
+
     def test_or2(self):
         db = FakeDB()
         db.connect()
@@ -160,7 +161,7 @@ class TestColumn(unittest.TestCase):
     def test_sql1(self):
         self.assertEqual(T1.Pass.sql(None), "`pass`")
 
-    
+
 class TestTable(unittest.TestCase):
     def test_table1(self):
         self.assertTrue(isinstance(T1.uid, adbs.Column))
@@ -200,7 +201,8 @@ class TestSQL(unittest.TestCase):
         db = FakeDB()
         db.connect()
         s, f = db._select_sql_(T1, T1.uid > 0)
-        self.assertEqual(s, "select `uid`, `pass`, `name` from t1 where `uid` > 0")
+        self.assertEqual(s, "select `uid`, `pass`, `name` "
+                            "from t1 where `uid` > 0")
         s, f = db._select_sql_(T2, T2.pwd > "")
         self.assertEqual(s, "select `passwd` from t2 where `passwd` > ''")
 
@@ -217,7 +219,8 @@ class TestSQL(unittest.TestCase):
         db.connect()
         t1 = T1(uid=1, Pass="p", name="n")
         s = db._insert_sql_(t1, None)
-        self.assertEqual(s, "insert into t1 (`uid`, `pass`, `name`) values (1, 'p', 'n')")
+        self.assertEqual(s, "insert into t1 (`uid`, `pass`, `name`) "
+                            "values (1, 'p', 'n')")
         t2 = T2(pwd="p")
         s = db._insert_sql_(t2, None)
         self.assertEqual(s, "insert into t2 (`passwd`) values ('p')")
@@ -227,19 +230,22 @@ class TestSQL(unittest.TestCase):
         db.connect()
         t1 = T1(uid=1, Pass="p", name="n")
         s = db._insert_sql_(t1.sel("Pass"), None)
-        self.assertEqual(s, "insert into t1 (`uid`, `pass`, `name`) values (1, 'p', 'n') "
+        self.assertEqual(s, "insert into t1 (`uid`, `pass`, `name`) "
+                            "values (1, 'p', 'n') "
                             "on duplicate key update `pass`=values(`pass`)")
         t2 = T2(pwd="p")
         s = db._insert_sql_(t2.sel("pwd"), None)
         self.assertEqual(s, "insert into t2 (`passwd`) values ('p') "
-                            "on duplicate key update `passwd`=values(`passwd`)")
+                            "on duplicate key update "
+                            "`passwd`=values(`passwd`)")
 
     def test_insert_sql3(self):
         db = FakeDB()
         db.connect()
         t1 = T1(uid=1, Pass="p", name="n")
         s = db._insert_sql_(t1, "pass=2")
-        self.assertEqual(s, "insert into t1 (`uid`, `pass`, `name`) values (1, 'p', 'n') "
+        self.assertEqual(s, "insert into t1 (`uid`, `pass`, `name`) "
+                            "values (1, 'p', 'n') "
                             "on duplicate key update pass=2")
         t2 = T2(pwd="p")
         s = db._insert_sql_(t2, "pwd=12")
@@ -255,8 +261,6 @@ class TestSQL(unittest.TestCase):
         t2 = T2(pwd="p")
         s = db._update_sql_(t2.sel("pwd"), T2.pwd > "")
         self.assertEqual(s, "update t2 set `passwd`='p' where `passwd` > ''")
-
-
 
 
 if __name__ == '__main__':
